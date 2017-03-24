74053843e5b6fbf5b7f6283ee4cc9bff0176b3cf
jersson.sanchez@correo.usa.edu.co <jersson.sanchez@correo.usa.edu.co>
2017-03-24 10:10:30 -0500

    6

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var User = require("./models/user");
 //mongoose
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://172.30.10.55:27017/Test");
+mongoose.connect("mongodb://<Jersson>:<paolasan2016>@ds141450.mlab.com:41450/usadb");
 
 //configuracion passport
 passport.use(new LocalStrategy(
